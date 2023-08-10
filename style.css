aa10aaaf40ab5545765eddfe8cc59c8a9ce72d74
gauravsingh8068 <141755826+gauravsingh8068@users.noreply.github.com>
2023-08-10 22:43:41 +0530

    style.css

---
+++ style.css
@@ -0,0 +1,23 @@
+function convertCelsiusToFahrenheit() {
+    const celsiusInput = document.getElementById("celsius").value;
+    if (celsiusInput === "") {
+        alert("Please enter a value in Celsius.");
+        return;
+    }
+
+    const celsiusValue = parseFloat(celsiusInput);
+    const fahrenheitValue = (celsiusValue * 9 / 5) + 32;
+    document.getElementById("fahrenheit").value = fahrenheitValue.toFixed(2);
+}
+
+function convertFahrenheitToCelsius() {
+    const fahrenheitInput = document.getElementById("fahrenheit").value;
+    if (fahrenheitInput === "") {
+        alert("Please enter a value in Fahrenheit.");
+        return;
+    }
+
+    const fahrenheitValue = parseFloat(fahrenheitInput);
+    const celsiusValue = (fahrenheitValue - 32) * 5 / 9;
+    document.getElementById("celsius").value = celsiusValue.toFixed(2);
+}
